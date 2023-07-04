33a3e66fe39008d5adb04f9c36e2acedce8a06a0
bk <naturalbb360@gmail.com>
2023-07-04 21:12:17 +0900

    1874 스택 수열

---
+++ bj/1874.py
@@ -0,0 +1,37 @@
+def input_func():
+    n = int(input())
+    arr = []
+    for _ in range(n):
+        arr.append(int(input()))
+    return n, arr
+
+
+def solution(n, arr):
+    cur_num = 1
+    stack_arr = []
+    ans = []
+    for i in range(n):
+        num = arr[i]
+        while cur_num <= num:
+            stack_arr.append(cur_num)
+            ans.append("+")
+            cur_num += 1
+
+        if stack_arr[-1] == num:
+            stack_arr.pop()
+            ans.append("-")
+        else: # top(arr[-1])보다 num이 더 큰경우
+            print("NO")
+            return
+
+    for calc in ans:
+        print(calc)
+
+
+def main():
+    n, arr = input_func()
+    solution(n, arr)
+
+
+if __name__ == '__main__':
+    main()
